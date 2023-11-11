6ea016c959e351167e41c344de1fb60a52fbfc94
samsoniy <150393018+samsoniy@users.noreply.github.com>
2023-11-11 21:59:15 +0100

    Update whatsapp copy.py

--- whatsapp copy.py
+++ whatsapp copy.py
@@ -10,48 +10,57 @@ import time
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 driver.get('https://web.whatsapp.com/')
-WebDriverWait(driver, 15)
-actions = ActionChains(driver)
 
 # this tells the script to wait until the search bar has been fully loaded
-wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div/div[4]/div/div[1]/div/div[2]/button/div[2]")))
+WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".to2l77zo")))
 
 #This finds the element the chats are contained in
 chatlist = driver.find_element(By.ID,'pane-side')
+#gets the visible height of the element the chats are in
+client_height = chatlist.get_property("clientHeight")
+#gets the total scrollheight of the window
+scroll_height = driver.execute_script('return document.scrollTop', chatlist)
+#gets the current scrolled position
+chatelement_scrollheight = driver.execute_script("return window.pageYOffset", chatlist)
 
-#gets the current height of the element the chats are in
-chatelement_height = chatlist.get_property("clientHeight")
-
-chatelement_scrollheight = chatlist.get_property("scrollHeight")
-
+myfile = open('chats.txt', 'a',newline='\n')
+readmyfile = open('chats.txt ','r')
+                  
 #function to find chats and get the chat name, check if it starts with a + and adds it to file containing chat names if true
-def get_chatnames():
-    #finds all instances of the element holding the chat title in current viewport
-    all_elements = driver.find_elements(By.CLASS_NAME, '_21S-L')
-counts = 0
+def get_chatnames(chatlist):
+    #len(all_elements) is 15. After looping 15 times the function should stop.
+    counts = 0
 
-    #For loop to go over every instance of a chat found and get the title
+    all_elements = driver.find_elements(By.CLASS_NAME, '_21S-L')
+
+    #finds all instances of the element holding the chat title in current viewport
     for element in all_elements:
+        counts += 1
+        print(counts)
         firstelement = element.find_element(By.TAG_NAME, 'span')
         title = firstelement.get_attribute('title')
-        if not title.startswith('+'):
-            counts += 1
-myfile = open('groupchat.txt', 'a')
-            myfile.write(title)
-myfile.close()
-            print(f'{title} has been added to the list')
-        else:
-print(f'{counts} groups have been added to the list.')
-#scrolls down in the element holding the chats
-driver.execute_script('document.getElementById("pane-side").scrollTop +='str(chatelement_height))
-break
+        if counts == 16:
+            break
+        elif title in readmyfile:
+            continue
+        elif not title.startswith('+') and title not in readmyfile:
+            print(f'{title} has been added to the list\n')
+            try:
+                myfile.write(title+'\n')
+            except:
+                UnicodeEncodeError
 
-while driver.execute_script('document.getElementById('pane-side').scrollTop') != chatelement_scrollheight:
-get_chatnames(chatlist)
-else:
-print('The bottom of the active chat list has been reached and all groups should have been added.')
-break
+        elif title.startswith('+'):
+            pass
+    else:
+        pass
 
+
+
+while True:
+        driver.execute_script(f'document.querySelector("#pane-side").scrollBy(0,404)')
+        get_chatnames(chatlist)
+        time.sleep(1)
 
 
 
