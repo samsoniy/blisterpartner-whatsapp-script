8d996340bb44ab7b60dae1dd9dc05f3f7af5c84e
samsoniy <150393018+samsoniy@users.noreply.github.com>
2023-11-12 23:46:53 +0100

    Update whatsapp copy.py

--- whatsapp copy.py
+++ whatsapp copy.py
@@ -19,48 +19,36 @@ chatlist = driver.find_element(By.ID,'pane-side')
 #gets the visible height of the element the chats are in
 client_height = chatlist.get_property("clientHeight")
 #gets the total scrollheight of the window
-scroll_height = driver.execute_script('return document.scrollTop', chatlist)
-#gets the current scrolled position
-chatelement_scrollheight = driver.execute_script("return window.pageYOffset", chatlist)
+totalheight = driver.execute_script('return document.querySelector("#pane-side").scrollHeight')
 
-myfile = open('chats.txt', 'a',newline='\n')
-readmyfile = open('chats.txt ','r')
-                  
+scroll = driver.execute_script('return document.querySelector("#pane-side").scrollTop')
+
 #function to find chats and get the chat name, check if it starts with a + and adds it to file containing chat names if true
 def get_chatnames(chatlist):
-    #len(all_elements) is 15. After looping 15 times the function should stop.
-    counts = 0
+    test = []
 
+    #finds all instances of the element holding the chat title in current viewport:
     all_elements = driver.find_elements(By.CLASS_NAME, '_21S-L')
-
-    #finds all instances of the element holding the chat title in current viewport
     for element in all_elements:
-        counts += 1
-        print(counts)
         firstelement = element.find_element(By.TAG_NAME, 'span')
         title = firstelement.get_attribute('title')
-        if counts == 16:
-            break
-        elif title in readmyfile:
-            continue
-        elif not title.startswith('+') and title not in readmyfile:
+        if not title.startswith('+') and title not in test:
+            test.append(title)
             print(f'{title} has been added to the list\n')
-            try:
-                myfile.write(title+'\n')
-            except:
-                UnicodeEncodeError
-
-        elif title.startswith('+'):
+        else:
             pass
     else:
-        pass
-
+        driver.execute_script(f'document.querySelector("#pane-side").scrollBy(0,806)')
+        for i in test:
+            try:
+                with open('chats.txt', 'a') as myfile:
+                    myfile.write(i+'\n')
+            except:
+                UnicodeEncodeError
+        else:
+            myfile.close()
 
 
 while True:
-        driver.execute_script(f'document.querySelector("#pane-side").scrollBy(0,404)')
-        get_chatnames(chatlist)
-        time.sleep(1)
-
-
+    get_chatnames(chatlist)
 
